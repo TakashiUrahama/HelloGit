6477bb2709a338a6ad11c96ef5c39d23e921f2cc
浦浜嵩 <takashi.urahama@gmail.com>
2017-03-14 02:33:44 +0900

    push testbranch

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,9 @@
 print('Hello, world')
+
 word = 'Hello, world'
 print(word)
+
+array = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd']
+for i in array:
+    print(i, end = '')
+print('')
